42167e8c229b9ac2d0bdf7481e89eb320807e794
qundeelsaleem <qundeelsaleem6@gmail.com>
2021-06-23 12:55:35 +0500

    new changes

--- Linked-packages/header.php
+++ Linked-packages/header.php
@@ -1,95 +1,108 @@
 
-            <div class="bg"></div>
-            <div class="container custom-container">
-                <div class="header-top-area t-header-top-area d-none d-lg-block">
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <div class="header-top-social">
-                                <ul>
-                                    <li>Follow</li>
-                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                                    <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-                                </ul>
+            <header class="header-style-four">
+            <div class="header-top-area s-header-top-area d-none d-lg-block">
+                <div class="container custom-container-two">
+                    <div class="row align-items-center">
+                        <div class="col-lg-6 d-none d-lg-block">
+                            <div class="header-top-offer">
+                                <p>Exclusive Black Friday ! Offer</p>
+                                <span class="coming-time" data-countdown="2021/3/15"></span>
                             </div>
                         </div>
-                        <div class="col-sm-6">
-                            <div class="header-top-login">
+                        <div class="col-lg-6">
+                            <div class="header-social">
+                                <span>Follow us on :</span>
                                 <ul>
-                                    <li><a href="#"><i class="far fa-edit"></i>Looking For Job</a></li>
+                                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
                                 </ul>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div id="sticky-header">
-                <div class="container custom-container">
+            <div id="sticky-header" class="header-four-wrap">
+                <div class="container custom-container-two">
                     <div class="row">
                         <div class="col-12">
                             <div class="main-menu menu-style-two">
                                 <nav>
-                                    <div class="logo d-block d-lg-none">
-                                        <a href="index-4.php"><img src="img/logo/logo.png" alt="Logo"></a>
+                                    <div class="logo">
+                                        <a href="index-4.html"><img src="img/logo/logo_two.png" alt="Logo"></a>
                                     </div>
-                                    <div class="navbar-wrap d-none d-lg-flex">
-                                    <ul class="left"> <div class="logo1">
-                                            <a href="index.php">
-                                                <img src="https://saraakuch.com/wp-content/uploads/2021/06/Saraakuch_logo.png" width="200px" height="70px" alt="Logo"></a>
-                                        </div>
-                                       
-                                            <li class="show"><a href="#">Home</a></li>
-                                            <li><a href="about-us.php">About</a></li>
-                                        <!-- </ul> -->
-                                      
-                                        <!-- <ul class="right"> -->
-                                                <li><a href="upcoming-games.php">Upcoming Games</a></li>
-                                            <li><a href="shop.php">Store</a></li>
-                                            <li><a href="#">Blog</a>
+                                    <div id="mobile-menu" class="navbar-wrap d-none d-lg-flex">
+                                        <ul>
+                                            <li class="show"><a href="#">Home</a>
                                                 <ul class="submenu">
-                                                    <li><a href="blog.php">News Page</a></li>
-                                                    <li><a href="blog-details.php">News Details</a></li>
+                                                    <li><a href="index-2.html">Home One</a></li>
+                                                    <li><a href="index-3.html">Home Two</a></li>
+                                                    <li class="active"><a href="index-4.html">Home Three</a></li>
+                                                    <li><a href="index.html">Home Four</a></li>
                                                 </ul>
                                             </li>
-                                            <li><a href="contact.php">contact</a></li>
+                                            <li><a href="#">Pages</a>
+                                                <ul class="submenu">
+                                                    <li><a href="about-us.html">About Story</a></li>
+                                                    <li><a href="upcoming-games.html">Upcoming Games</a></li>
+                                                    <li><a href="game-single.html">Game Single</a></li>
+                                                </ul>
+                                            </li>
+                                            <li><a href="game-overview.html">Overview</a></li>
+                                            <li><a href="community.html">Community</a></li>
+                                            <li><a href="shop.html">Store</a></li>
+                                            <li><a href="#">Blog</a>
+                                                <ul class="submenu">
+                                                    <li><a href="blog.html">News Page</a></li>
+                                                    <li><a href="blog-details.html">News Details</a></li>
+                                                </ul>
+                                            </li>
+                                            <li><a href="contact.html">contact</a></li>
                                         </ul>
                                     </div>
                                     <div class="header-action">
                                         <ul>
+                                            <li class="header-shop-cart"><a href="#"><i class="fas fa-shopping-basket"></i><span>0</span></a>
+                                                <ul class="minicart">
+                                                    <li class="d-flex align-items-start">
+                                                        <div class="cart-img">
+                                                            <a href="#">
+                                                                <img src="img/product/cart_p01.jpg" alt="">
+                                                            </a>
+                                                        </div>
+                                                        <div class="cart-content">
+                                                            <h4>
+                                                                <a href="#">Xbox One Controller</a>
+                                                            </h4>
+                                                            <div class="cart-price">
+                                                                <span class="new">$229.9</span>
+                                                                <span>
+                                                                    <del>$229.9</del>
+                                                                </span>
+                                                            </div>
+                                                        </div>
+                                                        <div class="del-icon">
+                                                            <a href="#">
+                                                                <i class="far fa-trash-alt"></i>
+                                                            </a>
+                                                        </div>
+                                                    </li>
+                                                    <li>
+                                                        <div class="total-price">
+                                                            <span class="f-left">Total:</span>
+                                                            <span class="f-right">$239.9</span>
+                                                        </div>
+                                                    </li>
+                                                    <li>
+                                                        <div class="checkout-link">
+                                                            <a href="cart.html">Shopping Cart</a>
+                                                            <a class="red-color" href="checkout.html">Checkout</a>
+                                                        </div>
+                                                    </li>
+                                                </ul>
+                                            </li>
                                             <li class="header-search"><a href="#" data-toggle="modal" data-target="#search-modal"><i class="fas fa-search"></i></a></li>
-                                        </ul>
-                                    </div>
-                                </nav>
-                            </div>
-                            <div class="mobile-menu-wrap d-block d-lg-none">
-                                <nav>
-                                    <div id="mobile-menu" class="navbar-wrap">
-                                        <ul>
-                                            <li class="show"><a href="#">Home</a>
-                                                <ul class="submenu">
-                                                    <li><a href="index-2.php">Home One</a></li>
-                                                    <li><a href="index-3.php">Home Two</a></li>
-                                                    <li><a href="index-4.php">Home Three</a></li>
-                                                    <li><a href="index.php">Home Four</a></li>
-                                                </ul>
-                                            </li>
-                                            <li><a href="about-us.php">About</a></li>
-                                            <li><a href="game-overview.php">Overview</a></li>
-                                            <li><a href="community.php">Community</a></li>
-                                            <li><a href="#">Pages</a>
-                                                <ul class="submenu">
-                                                    <li><a href="upcoming-games.php">Upcoming Games</a></li>
-                                                    <li><a href="game-single.php">Game Single</a></li>
-                                                </ul>
-                                            </li>
-                                            <li><a href="shop.php">Store</a></li>
-                                            <li><a href="#">Blog</a>
-                                                <ul class="submenu">
-                                                    <li><a href="blog.php">News Page</a></li>
-                                                    <li><a href="blog-details.php">News Details</a></li>
-                                                </ul>
-                                            </li>
-                                            <li><a href="contact.php">contact</a></li>
                                         </ul>
                                     </div>
                                 </nav>
@@ -110,4 +123,4 @@
                     </div>
                 </div>
             </div>
-            <div class="header-bottom-bg"></div>
+        </header>
--- Linked-packages/headerLinks.php
+++ Linked-packages/headerLinks.php
@@ -1,13 +1,14 @@
 <meta charset="utf-8">
+        
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>Shah Game World</title>
+        <title>Geco - eSports Gaming HTML5 Template</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-		<link rel="shortcut icon" type="image/x-icon" href="img/favicon.png">
+                <link rel="shortcut icon" type="image/x-icon" href="img/favicon.png">
         <!-- Place favicon.ico in the root directory -->
 
-		<!-- CSS here -->
+                <!-- CSS here -->
         <link rel="stylesheet" href="css/bootstrap.min.css">
         <link rel="stylesheet" href="css/animate.min.css">
         <link rel="stylesheet" href="css/magnific-popup.css">
