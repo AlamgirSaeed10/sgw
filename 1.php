4271e9f9dcdb66eb931cfdd73201e635e38d5427
AlamgirSaeed10 <muhammadalamgir10@gmail.com>
2021-07-02 13:17:29 +0500

    Update 1.php

--- 1.php
+++ 1.php
@@ -1,35 +1,40 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset='UTF-8'>
-<!-- <link rel='stylesheet' href='css/style.css'>	 -->
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
-  <style>
-  </style>
-</head>
+<?php include_once('Database/connection.php');
+if(isset($_POST['view'])){
+	$id = $_POST['id'];
+$query = "SELECT `image` FROM `latest_games` WHERE id='$id'";
+$result = mysqli_query($conn,$query);
+$row = mysqli_fetch_assoc($result);
+$images = $row['image'];
+$images = explode(',',$images);
+foreach($images AS $image){
+    echo '<img src="'.$image.'" height=100>';
+}
+}
 
-<body>
+if(isset($_POST['submit'])){
+
+	$targetDir ="test/";
+	$image = $_FILES['files']['temp_name'];
+	$file_name = implode(',',$image);
+if(!empty($image)){
+	foreach ($image as $key => $value) {
+		$targetFilePath = $targetDir . $value;
+		echo "".$targetFilePath;
+		move_uploaded_file($_FILES['files']['temp_name'][$key], $targetFilePath); 
+	}
+ 	$sql = "INSERT INTO `latest_games`(`image`) VALUES ('{$file_name}')";
+ 	$image = mysqli_query($conn,$sql);
+
+	 if($image > 0){
+		 echo "good";
+	 }
+}
   
-		
-    <div id="nav">
-		   	<img src="img/logo/SHAH-GAME-WORLD.png" alt="" height="150">
-			
-		   	<audio id="beep" preload="auto">
-				<source src="img/audio/lionroar.mp3"></source>
-			</audio>
-			
-    </div>
-		
-		<script>
-		
-		var beepOne = $("#beep")[0];
-		$("#nav")
-	.click(function() {
-		beepOne.play();
-	});
-	
-	</script>
-				
-</body>
-
-</html>
+}
+?>
+<form method="POST" action="1.php" enctype="multipart/form-data">
+        <input type="file" name="files[]" multiple accept="image/*">
+        <input type="input" name="id" required>
+        <input type="submit" name="submit" value="ADD">
+        <input type="submit" name="view" value="view">
+    </form>
